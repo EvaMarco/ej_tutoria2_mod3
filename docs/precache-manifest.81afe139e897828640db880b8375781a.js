2e9ec7671c611e7486b7244bd88c81c4389d27b4
Migara <evamarcod@gmail.com>
2019-09-17 08:03:39 +0200

    Create Docs folder

---
+++ docs/precache-manifest.81afe139e897828640db880b8375781a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d560da461db5855f5bb0ef1c559dbd25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9914b2ffb9935caa1094",
+    "url": "/static/css/main.b8f4475f.chunk.css"
+  },
+  {
+    "revision": "ec68e658e77ffd234d10",
+    "url": "/static/js/2.568d4954.chunk.js"
+  },
+  {
+    "revision": "9914b2ffb9935caa1094",
+    "url": "/static/js/main.5f116dee.chunk.js"
+  },
+  {
+    "revision": "174afe81b85038aa59a9",
+    "url": "/static/js/runtime~main.0744dd46.js"
+  },
+  {
+    "revision": "3b3d06594ca1e5fdb1a01d6f3b132e4f",
+    "url": "/static/media/NoCurvesBoustrophedon.3b3d0659.ttf"
+  },
+  {
+    "revision": "fb23b9494aa316129a675d4d660a9c36",
+    "url": "/static/media/NoCurvesBoustrophedon.fb23b949.woff"
+  },
+  {
+    "revision": "b5c48ec9285ec01e80944f79dbe33ae7",
+    "url": "/static/media/logo.b5c48ec9.png"
+  }
+]);
